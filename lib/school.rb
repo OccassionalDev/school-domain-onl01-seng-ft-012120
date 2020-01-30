b69c0fe73e682af23174a8b20dd6b2e8d82c059c
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-30 17:47:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+  attr_reader :school_name, :roster
+  
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student_name
+      
+    else 
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end 
+  
+  def grade(class_grade)
+    return @roster[class_grade]
+  end 
+  
+  def sort
+    @roster = @roster.collect do |grade, student_list|
+      student_list.sort
+    end 
+  end
+end 
